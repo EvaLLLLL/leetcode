530183abb11869f37d5e72bbd8b9b21949730c90
lyhhh <17610603221@163.com>
2022-04-14 19:32:31 +0800

    update

--- src/5.最长回文子串.ts
+++ src/5.最长回文子串.ts
@@ -6,48 +6,31 @@
 
 // @lc code=start
 function longestPalindrome(s: string): string {
-  if (s.length === 1) return s
-
-  if (s.length === 2) {
-    return s[0] === s[1] ? s : s[0]
-  }
-
-  const isPalindrome = (str: string) => {
-    for (let i = 0; i < str.length / 2; i++) {
-      if (str[i] !== str[str.length - i - 1]) {
-        return false
+  const len = s.length
+  let [left, right] = [0, 1]
+  let pre: number[] = []
+  let cur: number[] = [0, 1]
+  
+  for (let i = 0; i < len; i++) {
+    for (let j of pre) {
+      if (s[i] === s[j - 1]) {
+        cur.push(j - 1)
+        if (i - j + 2 > right - left) [left, right] = [j - 1, i + 1]
       }
     }
 
-    return true
+    ;[cur, pre] = [[i + 1, i + 2], cur]
   }
 
-  const findMax = (strs: string[]) => {
-    let max = 0
-    const nums = strs.map(s => Number(s))
-    for (let i = 0; i < nums.length; i++) {
-      max = nums[i] >= max ? nums[i] : max
-    }
-    return max.toString()
-  }
-
-  const resultMap: { [key: string]: string[] } = {}
-
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j < s.length + 1; j++) {
-      const currentStr = s.substring(i, j)
-
-      if (isPalindrome(currentStr)) {
-        if (resultMap[currentStr.length]) {
-          resultMap[currentStr.length].push(currentStr)
-        } else {
-          resultMap[currentStr.length] = [currentStr]
-        }
-      }
-    }
-  }
-
-  const maxStrNum = findMax(Object.keys(resultMap))
-  return maxStrNum ? resultMap[maxStrNum][0] : s[0]
+  return s.slice(left, right)
 }
+
 // @lc code=end
+
+console.log(longestPalindrome('adam'))
+console.log(longestPalindrome('babad'))
+console.log(longestPalindrome('a'))
+console.log(longestPalindrome('aaaaa'))
+console.log(longestPalindrome('cbbd'))
+console.log(longestPalindrome('aaabaaaa'))
+console.log(longestPalindrome('tattarrattat'))
